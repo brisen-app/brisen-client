e502f240a2083c31384c08687bf93a5c6875e2b2
Ole <ole@kallerud.no>
2024-01-26 20:48:46 +0100

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,18 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import { config } from 'dotenv';
+
+
+config();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: false,
+        persistSession: false,
+        detectSessionInUrl: false,
+    },
+});
