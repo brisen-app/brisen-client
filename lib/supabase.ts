f295ba220ea5c96dc539705258a240f3446205be
Ole <ole@kallerud.no>
2024-01-26 21:48:07 +0100

    Uninstall dotenv

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { config } from 'dotenv';
 
 
-config();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
