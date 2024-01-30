a6b6414db88b0885e1f36c253b81566fe294b34d
Ole Kallerud <ole@kallerud.no>
2024-01-30 23:51:12 +0100

    Create unified fonts

--- components/ActivityIndicator.tsx
+++ components/ActivityIndicator.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { ActivityIndicator as ActInd } from "react-native";
 import { BlurTint, BlurView } from 'expo-blur';
-import { Text, View, useThemeColor } from './Themed';
-
-import Colors from '@/constants/Colors';
-import { useColorScheme } from "./useColorScheme.web";
+import { useThemeColor } from './Themed';
+import { useColorScheme } from "./useColorScheme";
+import { Headline } from "./TextStyles";
 
 export function ActivityIndicator({ label = "Loading..." }) {
     return (
-        <BlurView tint={(useColorScheme() ?? 'dark') as BlurTint} style={{
+        <BlurView tint={(useColorScheme()) as BlurTint} style={{
             alignSelf: 'center',
             borderRadius: 16,
             overflow: 'hidden',
-            padding: 32,
-            paddingHorizontal: 64,
+            padding: 24,
+            paddingHorizontal: 48,
         }}>
             <ActInd size="large" color={useThemeColor({}, "accentColor")}/>
-            <Text style={{ textAlign: 'center' }}>{label}</Text>
+            <Headline>{label}</Headline>
         </BlurView>
     );
 }
