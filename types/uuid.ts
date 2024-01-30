eacbbb963d5284aad6f881d34bcfa8860d252416
Ole <ole@kallerud.no>
2024-01-30 18:57:57 +0100

    Create uuid.ts

---
+++ types/uuid.ts
@@ -0,0 +1,11 @@
+export class UUID extends String {
+    private static uuidPattern = RegExp(/^[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}$/)
+    private static allUUIDs: Set<UUID> = new Set()
+
+    constructor(string: string) {
+        if (!UUID.uuidPattern.test(string)) throw new TypeError(`Invalid UUID: '${string}'`)
+        if (UUID.allUUIDs.has(string)) throw new TypeError(`Duplicate UUID: '${string}'`)
+        super(string)
+        UUID.allUUIDs.add(this)
+    }
+}
