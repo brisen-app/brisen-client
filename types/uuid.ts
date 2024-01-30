28224e5a7a722e28858bed253560b1d996f185df
Ole Kallerud <ole@kallerud.no>
2024-01-30 21:06:00 +0100

    Update uuid.ts

--- types/uuid.ts
+++ types/uuid.ts
@@ -1,6 +1,6 @@
 export class UUID extends String {
     private static uuidPattern = RegExp(/^[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}$/)
-    private static allUUIDs: Set<UUID> = new Set()
+    private static allUUIDs: Set<string> = new Set()
 
     get string(): string { return this.toString() }
 
@@ -8,6 +8,6 @@ export class UUID extends String {
         if (!UUID.uuidPattern.test(string)) throw new TypeError(`Invalid UUID: '${string}'`)
         if (UUID.allUUIDs.has(string)) throw new TypeError(`Duplicate UUID: '${string}'`)
         super(string)
-        UUID.allUUIDs.add(this)
+        UUID.allUUIDs.add(string)
     }
 }
