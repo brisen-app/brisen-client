34d7294606ff1eea74394ea0a35e8ad7dbc1bc31
Ole <ole@kallerud.no>
2024-02-04 16:39:39 +0100

    Create Language.ts

---
+++ types/Language.ts
@@ -0,0 +1,20 @@
+import SupabaseEntity from "./SupabaseEntity";
+import { NativeModules, Platform } from 'react-native';
+
+export default class Language extends SupabaseEntity {
+    protected static readonly tableName: string = "languages"
+
+    get id() { return super.id as string }
+    get icon() { return this.data.icon as string }
+    get title() { return this.data.title as string }
+
+    static async getDeviceLanguage(): Promise<Language> {
+        const deviceLanguageCode = (Platform.OS === 'ios'
+            ? (NativeModules.SettingsManager.settings.AppleLocale as string).slice(0, 2).toLowerCase()
+            : (NativeModules.I18nManager?.localeIdentifier as string | null) ?? "en");
+        
+        const languages = await this.fetchAll();
+
+        return languages.find(l => l.id === deviceLanguageCode) ?? languages[0];
+    }
+}
