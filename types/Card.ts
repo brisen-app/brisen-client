52f2de969fbda83c7c6f0d635087839aa4c66eb2
Ole <ole@kallerud.no>
2024-02-04 16:40:34 +0100

    Update Card.ts

--- types/Card.ts
+++ types/Card.ts
@@ -1,9 +1,14 @@
 import SupabaseEntity from "./SupabaseEntity";
+import Category from "./Category";
 import UUID from "./uuid"
 
 export default class Card extends SupabaseEntity {
     protected static readonly tableName: string = "cards"
     get id() { return super.id as UUID }
-    get header() { return this.data.header as string | null }
+    private get header() { return this.data.header as string | null }
     get content() { return this.data.content as string }
+    get category() { return (async () => {
+        if (!this.data.category) return null;
+        return await Category.fetch(this.data.category)
+    })()}
 }
