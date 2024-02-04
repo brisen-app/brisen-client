6829ef874af2057febf72ac0bac6cb115a8fa6a4
Ole <ole@kallerud.no>
2024-02-04 16:40:31 +0100

    Update Category.ts

--- types/Category.ts
+++ types/Category.ts
@@ -1,8 +1,13 @@
 import SupabaseEntity from "./SupabaseEntity";
+import Localization from "./Localization";
 
 export default class Category extends SupabaseEntity {
     protected static readonly tableName: string = "categories"
+
     get id() { return super.id as string }
     get icon() { return this.data.icon as string }
     get color() { return this.data.color as string | null }
+    get title() { return (async () => {
+        return await Localization.get(`${this.id}_title`)
+    })()}
 }
