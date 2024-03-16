0729fc779024e313dd2c74200795449a6541869a
Ole <ole@kallerud.no>
2024-03-16 16:17:02 +0100

    Force dark mode

--- components/useColorScheme.ts
+++ components/useColorScheme.ts
@@ -8,5 +8,6 @@ export type ColorSchemeName = 'light' | 'dark';
  * Defaults to 'dark' if the user's preference is not available.
  */
 export default function useColorScheme(): ColorSchemeName {
-    return defaultFunc() ?? 'dark';
+    // return defaultFunc() ?? 'dark';
+    return 'dark';
 }
