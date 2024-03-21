c7332cab7348d4687a85b31618abb73213fa0287
Ole Kallerud <ole@kallerud.no>
2024-03-22 00:10:15 +0100

    Update HUD View

--- components/MenuView.tsx
+++ components/MenuView.tsx
@@ -1,10 +1,13 @@
-import { Pressable, StyleSheet } from 'react-native'
-import { LocalizedText } from './utils/LocalizedText'
-import useColorScheme from './utils/useColorScheme'
+import { Pressable, StyleSheet, View, ViewProps } from 'react-native'
+import { LocalizedText, LocalizedTextProps } from './utils/LocalizedText'
 import { PackManager } from '@/lib/PackManager'
 import GridContainer from './utils/GridContainer'
 import { BottomSheetScrollView, useBottomSheet } from '@gorhom/bottom-sheet'
 import UserQuickView from './user/UserQuickView'
+import { Text } from './utils/Themed'
+import Colors from '@/constants/Colors'
+import useColorScheme from './utils/useColorScheme'
+import { FontStyles, Styles } from '@/constants/Styles'
 
 export default function MenuView() {
     const colorScheme = useColorScheme()
@@ -26,7 +29,50 @@ export default function MenuView() {
 }
 
 function HUDView() {
-    return <UserQuickView size={64} showDetails style={{ maxWidth: '30%' }} />
+    return (
+        <View
+            style={{
+                flexDirection: 'row',
+                // justifyContent: 'space-between',
+                // alignItems: 'center',
+                backgroundColor: 'white',
+            }}
+        >
+            <UserQuickView showDetails style={{ flex: 1, backgroundColor: 'blue', width: '90%' }} />
+            <View style={{ flex: 1 }} />
+            <ValueView id="packs" value={6} />
+            <ValueView id="packs" value={17} />
+            <ValueView id="packs" value={63} />
+        </View>
+    )
+}
+
+type ValueViewProps = {
+    value: number
+} & LocalizedTextProps &
+    ViewProps
+
+function ValueView(props: Readonly<ValueViewProps>) {
+    const { value, style, ...localizedTextProps } = props
+    const colorScheme = useColorScheme()
+
+    return (
+        <View
+            style={[
+                {
+                    backgroundColor: 'purple',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 8,
+                    ...Styles.shadow,
+                },
+                style,
+            ]}
+        >
+            <LocalizedText {...localizedTextProps} style={FontStyles.Subheading} textCase="uppercase" />
+            <Text style={{ color: Colors[colorScheme].accentColor, ...FontStyles.LargeTitle }}>{value}</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
--- components/user/UserQuickView.tsx
+++ components/user/UserQuickView.tsx
@@ -5,7 +5,7 @@ import { Image } from 'expo-image'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import useColorScheme from '../utils/useColorScheme'
 import { useMemo } from 'react'
-import { View, ViewProps } from 'react-native'
+import { DimensionValue, TouchableOpacityProps, View, ViewProps } from 'react-native'
 import { Text } from '../utils/Themed'
 import { FontStyles, Styles } from '@/constants/Styles'
 
@@ -37,12 +37,12 @@ export type UserQuickViewProps = {
     //     username: string
     //     image?: string
     // }
-    size?: number
+    height?: DimensionValue
     showDetails?: boolean
-} & ViewProps
+} & TouchableOpacityProps
 
 export default function UserQuickView(props: Readonly<UserQuickViewProps>) {
-    const { size = 48, showDetails, style } = props
+    const { height = 48, showDetails, style } = props
     const colorScheme = useColorScheme()
 
     const user = useMemo(() => {
@@ -54,18 +54,19 @@ export default function UserQuickView(props: Readonly<UserQuickViewProps>) {
             onPress={() => {
                 console.log(`User ${user.name} tapped`)
             }}
-            style={{
-                flexDirection: 'row',
-                height: size,
-                gap: 8,
-                ...Styles.shadow,
-                ...(style as object),
-            }}
+            style={[
+                {
+                    flexDirection: 'row',
+                    height: height,
+                    gap: 8,
+                    ...Styles.shadow,
+                },
+                style,
+            ]}
         >
             <View
                 style={{
-                    height: size,
-                    borderRadius: size / 2,
+                    borderRadius: 1000,
                     overflow: 'hidden',
                     borderColor: Colors[colorScheme].stroke,
                     borderWidth: Sizes.thin,
@@ -87,7 +88,9 @@ export default function UserQuickView(props: Readonly<UserQuickViewProps>) {
 
             {showDetails && (
                 <View style={{ justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{user.name}</Text>
+                    <Text numberOfLines={1} lineBreakMode='tail' style={FontStyles.Title}>
+                        {user.name}
+                    </Text>
                     <Text
                         numberOfLines={1}
                         style={{
