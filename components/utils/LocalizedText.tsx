fe3d824bb1185882fd5d91844283d5092d5454ea
Ole Kallerud <ole@kallerud.no>
2024-03-22 00:09:58 +0100

    Update LocalizedText.tsx

--- components/utils/LocalizedText.tsx
+++ components/utils/LocalizedText.tsx
@@ -9,18 +9,34 @@ type LocalizationProps = {
     id: string
     forcePlaceholder?: boolean
     placeHolderStyle?: PlaceholderProps
+    textCase?: 'uppercase' | 'lowercase' | 'capitalize'
 }
 
 export type LocalizedTextProps = LocalizationProps & TextProps
 
 export function LocalizedText(props: LocalizedTextProps) {
     // const { language } = useContext(LanguageContext);
-    const { id, placeHolderStyle, forcePlaceholder, ...otherProps } = props
+    const { id, placeHolderStyle, forcePlaceholder, textCase, ...otherProps } = props
 
     const { data: localization, error, isLoading } = useQuery(LocalizationManager.getFetchQuery(id))
 
     if (error) console.warn(error)
     if (isLoading || forcePlaceholder) return <Placeholder {...placeHolderStyle} />
 
-    return <Text {...otherProps}>{localization?.value ?? id}</Text>
+    if (!localization?.value) return <Text {...otherProps}>{id}</Text>
+
+    let value = localization.value
+    switch (textCase) {
+        case 'uppercase':
+            value = localization.value.toUpperCase()
+            break
+        case 'lowercase':
+            value = localization.value.toLowerCase()
+            break
+        case 'capitalize':
+            value = localization.value.charAt(0).toUpperCase() + localization.value.slice(1)
+            break
+    }
+
+    return <Text {...otherProps}>{value}</Text>
 }
