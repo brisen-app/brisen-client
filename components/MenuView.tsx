51dc4584c3ebfa0705177114d5b248e1c7b0e0a5
Ole <ole@kallerud.no>
2024-03-30 17:06:05 +0100

    Remove tap to expand

--- components/MenuView.tsx
+++ components/MenuView.tsx
@@ -1,7 +1,7 @@
-import { BottomSheetScrollView, useBottomSheet } from '@gorhom/bottom-sheet'
+import { BottomSheetScrollView } from '@gorhom/bottom-sheet'
 import { LocalizedText } from './utils/LocalizedText'
 import { PackManager } from '@/lib/PackManager'
-import { Pressable, StyleSheet, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -11,43 +11,42 @@ import Colors from '@/constants/Colors'
 import useColorScheme from './utils/useColorScheme'
 
 export default function MenuView() {
-    const { expand } = useBottomSheet()
     const insets = useSafeAreaInsets()
     const colorScheme = useColorScheme()
 
     const { data: packs, error } = useQuery(PackManager.getFetchAllQuery())
     if (error) console.warn(error)
 
-    useEffect(() => {
-        console.log(insets.top)
-    }, [insets])
-
     return (
         <BottomSheetScrollView showsVerticalScrollIndicator={false}>
-            <Pressable onPress={() => expand()} style={{ paddingBottom: insets.bottom ?? 16 }}>
-                <LocalizedText id="packs" style={styles.header} placeHolderStyle={{ height: 28, width: 128 }} />
+            <LocalizedText id="packs" style={styles.header} placeHolderStyle={{ height: 28, width: 128 }} />
 
-                {packs &&
-                    packs.map((pack, index) =>
-                        index === -1 ? (
-                            <PackFeaturedView key={pack.id} pack={pack} style={{ paddingHorizontal: 16, paddingBottom: 16 }} />
-                        ) : (
-                            <View key={pack.id}>
-                                <PackListView pack={pack} style={{ height: 80, paddingHorizontal: 16 }} />
-                                {index < packs.length - 1 ? (
-                                    <View
-                                        style={{
-                                            borderTopColor: Colors[colorScheme].stroke,
-                                            borderTopWidth: StyleSheet.hairlineWidth,
-                                            marginVertical: 8,
-                                            marginLeft: 80 + 8 + 16,
-                                        }}
-                                    />
-                                ) : null}
-                            </View>
-                        )
-                    )}
-            </Pressable>
+            {packs &&
+                packs.map((pack, index) =>
+                    index === -1 ? (
+                        <PackFeaturedView
+                            key={pack.id}
+                            pack={pack}
+                            style={{ paddingHorizontal: 16, paddingBottom: 16 }}
+                        />
+                    ) : (
+                        <View key={pack.id}>
+                            <PackListView pack={pack} style={{ height: 80, paddingHorizontal: 16 }} />
+                            {index < packs.length - 1 ? (
+                                <View
+                                    style={{
+                                        borderTopColor: Colors[colorScheme].stroke,
+                                        borderTopWidth: StyleSheet.hairlineWidth,
+                                        marginVertical: 8,
+                                        marginLeft: 80 + 8 + 16,
+                                    }}
+                                />
+                            ) : null}
+                        </View>
+                    )
+                )}
+
+            <View style={{ height: insets.bottom ?? 16 }} />
         </BottomSheetScrollView>
     )
 }
