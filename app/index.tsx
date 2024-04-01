a37e858ffc63b8d061677d0334efe8eaba7e2262
Ole Kallerud <ole@kallerud.no>
2024-04-01 18:26:16 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { CategoryManager } from '@/lib/CategoryManager'
 import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet'
 import { useQuery } from '@tanstack/react-query'
 import { useCallback, useMemo, useRef } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function App() {
@@ -15,54 +15,8 @@ export default function App() {
     const bottomSheetRef = useRef<BottomSheet>(null)
     const snapPoints = useMemo(() => [insets.bottom + 64], [bottomSheetRef, insets])
 
-    // Initial setup
-    // const queryClient = useQueryClient()
-
-    // const { data: languages, error, isLoading: isLoadingLanguage } = useQuery({
-    // 	queryKey: [Language.tableName],
-    // 	queryFn: async () => {
-    // 		return await Language.fetchAll()
-    // 	}
-    // })
-
-    // languages?.forEach(item => {
-    // 	queryClient.setQueryData([Language.tableName, item.id], item)
-    // })
-
-    // const { data: packs, isLoading } = useQuery({
-    // 	queryKey: [Pack.tableName],
-    // 	queryFn: async () => {
-    // 		return await Pack.fetchAll()
-    // 	}
-    // })
-
-    // packs?.forEach(item => {
-    // 	queryClient.setQueryData([Pack.tableName, item.id], item)
-    // })
-
-    const { data: categories, isLoading: isLoadingCategories } = useQuery(CategoryManager.getFetchAllQuery())
-    if (categories) {
-        CategoryManager.set(categories)
-    }
-
-    // const language = languages ? Language.findDeviceLanguage(languages) : null
-    // const { data: localizations, isLoading: isLoadingLocalizations } = useQuery({
-    // 	queryKey: language ? [Localization.tableName, language.id] : [],
-    // 	queryFn: async () => {
-    // 		return await Localization.fetchAllWithLang(language!)
-    // 	},
-    // 	enabled: !!language
-    // })
-
-    // localizations?.forEach(item => {
-    // 	queryClient.setQueryData([Localization.tableName, language!.id, item.id], item)
-    // })
-
-    // if (isLoading) return (
-    // 	<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    // 		<ActivityIndicator color={"white"} size={"large"} />
-    // 	</View>
-    // )
+    const { data: categories } = useQuery(CategoryManager.getFetchAllQuery())
+    if (categories) CategoryManager.set(categories)
 
     const backdrop = useCallback(
         (props: any) => (
