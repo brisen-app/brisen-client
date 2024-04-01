91334493c0e73f884fd4f5f2b5f41b78357f9ac0
Ole Kallerud <ole@kallerud.no>
2024-04-01 21:09:12 +0200

    Implement retrieving secrets from Expo

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,11 +2,18 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/supabase'
+import Constants from 'expo-constants'
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SB_URL!
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SB_ANON!
+let supabaseUrl = Constants.expoConfig?.extra?.supabaseURL as string | undefined
+let supabaseAnon = Constants.expoConfig?.extra?.supabaseAnon as string | undefined
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+if (!supabaseUrl || !supabaseAnon) {
+    console.warn(`Supabase URL or Anon Key not found in Constants.expoConfig.extra. Falling back to environment variables.`)
+    supabaseUrl = process.env.EXPO_PUBLIC_SB_URL!
+    supabaseAnon = process.env.EXPO_PUBLIC_SB_ANON!
+}
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnon, {
     auth: {
         storage: AsyncStorage,
         autoRefreshToken: false,
