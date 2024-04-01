56e8353eda7d3439fd1accc1431c3bfad5f4bdc1
Ole Kallerud <ole@kallerud.no>
2024-04-01 22:25:32 +0200

    Revert EAS secrets implementation

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,15 +2,12 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/supabase'
-import Constants from 'expo-constants'
 
-let supabaseUrl = Constants.expoConfig?.extra?.supabaseURL as string | undefined
-let supabaseAnon = Constants.expoConfig?.extra?.supabaseAnon as string | undefined
+let supabaseUrl = process.env.EXPO_PUBLIC_SB_URL
+let supabaseAnon = process.env.EXPO_PUBLIC_SB_ANON
 
 if (!supabaseUrl || !supabaseAnon) {
-    console.warn(`Supabase URL or Anon Key not found in Constants.expoConfig.extra. Falling back to environment variables.`)
-    supabaseUrl = process.env.EXPO_PUBLIC_SB_URL!
-    supabaseAnon = process.env.EXPO_PUBLIC_SB_ANON!
+    throw new Error('Missing Supabase URL or Anon Key')
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnon, {
