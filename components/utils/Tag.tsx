7d0249c7658fee9cd1e3c38221706adf19037277
Ole Kallerud <ole@kallerud.no>
2024-06-11 22:57:04 +0200

    Add inout transition animations to Tag
    fix

--- components/utils/Tag.tsx
+++ components/utils/Tag.tsx
@@ -1,8 +1,18 @@
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
-import { Text } from './Themed'
+import React, { useEffect } from 'react'
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import Animated, {
+  useSharedValue,
+  FadeIn,
+  FadeOut,
+  EntryExitTransition,
+  FadeInUp,
+  Easing,
+  ZoomOutDown,
+  ZoomOut,
+} from 'react-native-reanimated'
 import Colors from '@/constants/Colors'
 import useColorScheme from './useColorScheme'
-import { MaterialIcons } from '@expo/vector-icons'
 import { FontStyles } from '@/constants/Styles'
 
 export type TagListProps = {
@@ -11,28 +21,34 @@ export type TagListProps = {
 } & TouchableOpacityProps
 
 export default function Tag(props: Readonly<TagListProps>) {
-  const { text, hideIcon, style } = props
+  const { text, hideIcon, style, onPress } = props
   const colorScheme = useColorScheme()
 
   return (
-    <TouchableOpacity
-      {...props}
-      style={[
-        {
-          flexDirection: 'row',
-          backgroundColor: Colors[colorScheme].background,
-          paddingVertical: 6,
-          paddingHorizontal: 10,
-          borderRadius: 8,
-          alignItems: 'center',
-          overflow: 'hidden',
-          gap: 4,
-        },
-        style,
-      ]}
+    <Animated.View
+      entering={FadeInUp.easing(Easing.out(Easing.quad))}
+      exiting={ZoomOut.easing(Easing.out(Easing.quad))}
     >
-      {!hideIcon ? <MaterialIcons name="close" size={16} color={Colors[colorScheme].secondaryText} /> : null}
-      <Text style={[FontStyles.AccentuatedBody, { color: Colors[colorScheme].text }]}>{text}</Text>
-    </TouchableOpacity>
+      <TouchableOpacity
+        {...props}
+        style={[
+          {
+            flexDirection: 'row',
+            backgroundColor: Colors[colorScheme].background,
+            paddingVertical: 6,
+            paddingHorizontal: 10,
+            borderRadius: 8,
+            alignItems: 'center',
+            overflow: 'hidden',
+            gap: 4,
+          },
+          style,
+        ]}
+        onPress={onPress}
+      >
+        {!hideIcon ? <MaterialIcons name="close" size={16} color={Colors[colorScheme].secondaryText} /> : null}
+        <Text style={[FontStyles.AccentuatedBody, { color: Colors[colorScheme].text }]}>{text}</Text>
+      </TouchableOpacity>
+    </Animated.View>
   )
 }
