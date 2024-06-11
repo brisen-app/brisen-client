6bae64de98363d3f2a0af3e1d2bfd2dd43d5f082
Ole Kallerud <ole@kallerud.no>
2024-06-11 21:10:05 +0200

    Implement animation when cards appear

--- components/card/CardScreen.tsx
+++ components/card/CardScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Dimensions, PressableProps, Pressable } from 'react-native'
+import { StyleSheet, Dimensions, PressableProps, Pressable, ViewToken } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Colors from '@/constants/Colors'
 import Sizes from '@/constants/Sizes'
@@ -7,12 +7,25 @@ import Color from '@/models/Color'
 import { PlayedCard } from '@/lib/CardManager'
 import { CategoryManager } from '@/lib/CategoryManager'
 import { CardView } from './CardView'
+import Animated, { Easing, SharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated'
 
-export type CardScreenProps = { card: PlayedCard } & PressableProps
+export type CardScreenProps = { card: PlayedCard; viewableItems: SharedValue<ViewToken<PlayedCard>[]> } & PressableProps
 
 export default function CardScreen(props: Readonly<CardScreenProps>) {
-  const { card, onPress } = props
+  const { card, onPress, viewableItems } = props
   const colorScheme = useColorScheme()
+  const animationConfig = { duration: 400, easing: Easing.bezier(0, 0, 0.5, 1) }
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const isVisible = !!viewableItems.value.find(
+      (viewableItem) => viewableItem.isViewable && viewableItem.item.id === card.id
+    )
+
+    return {
+      opacity: withTiming(isVisible ? 1 : 0, animationConfig),
+      transform: [{ scale: withTiming(isVisible ? 1 : 0.9, animationConfig) }],
+    }
+  }, [])
 
   const padding = 16
   let insets = useSafeAreaInsets()
@@ -37,20 +50,23 @@ export default function CardScreen(props: Readonly<CardScreenProps>) {
         paddingRight: insets.right,
       }}
     >
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          overflow: 'hidden',
-          borderRadius: 32,
-          backgroundColor: Colors[colorScheme].secondaryBackground,
-          borderColor: Colors[colorScheme].stroke,
-          borderWidth: Sizes.thin,
-        }}
+      <Animated.View
+        style={[
+          {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            overflow: 'hidden',
+            borderRadius: 32,
+            backgroundColor: Colors[colorScheme].secondaryBackground,
+            borderColor: Colors[colorScheme].stroke,
+            borderWidth: Sizes.thin,
+          },
+          animatedStyle,
+        ]}
       >
         <CardView card={card} category={category} />
-      </View>
+      </Animated.View>
     </Pressable>
   )
 }
