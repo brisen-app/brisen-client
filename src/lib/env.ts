1cd01b6814b63812f2585e0c9987cbee75476ebd
Ole Kallerud <ole@kallerud.no>
2024-09-26 00:38:29 +0200

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,22 +1,20 @@
-export default function environmentVariables(buildProfile = process.env.EAS_BUILD_PROFILE) {
-  if (!buildProfile) throw new Error('EAS_BUILD_PROFILE is not defined')
+export default function environmentVariables() {
+  const isDev = process.env.EAS_BUILD_PROFILE === 'development' || __DEV__
 
-  const rcAppleKey = process.env.EXPO_PUBLIC_RC_APPLE_KEY
-  if (!rcAppleKey) throw new Error('EXPO_PUBLIC_RC_APPLE_KEY is not defined')
-  const rcGoogleKey = process.env.EXPO_PUBLIC_RC_GOOGLE_KEY
-  if (!rcGoogleKey) throw new Error('EXPO_PUBLIC_RC_GOOGLE_KEY is not defined')
+  const rcAppleKey = process.env.EXPO_PUBLIC_RC_KEY_APPLE
+  if (!rcAppleKey) throw new Error('EXPO_PUBLIC_RC_KEY_APPLE is not defined')
+  const rcGoogleKey = process.env.EXPO_PUBLIC_RC_KEY_GOOGLE
+  if (!rcGoogleKey) throw new Error('EXPO_PUBLIC_RC_KEY_GOOGLE is not defined')
 
   let supabaseUrl = undefined
   let supabaseAnon = undefined
 
-  if (buildProfile === 'production') {
-    supabaseUrl = process.env.EXPO_PUBLIC_SB_URL_PROD
-    supabaseAnon = process.env.EXPO_PUBLIC_SB_ANON_PROD
-  }
-
-  if (buildProfile === 'develop') {
+  if (isDev) {
     supabaseUrl = process.env.EXPO_PUBLIC_SB_URL_DEV
     supabaseAnon = process.env.EXPO_PUBLIC_SB_ANON_DEV
+  } else {
+    supabaseUrl = process.env.EXPO_PUBLIC_SB_URL_PROD
+    supabaseAnon = process.env.EXPO_PUBLIC_SB_ANON_PROD
   }
 
   if (!supabaseUrl) throw new Error('EXPO_PUBLIC_SB_URL is not defined')
