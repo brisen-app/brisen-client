346292c7b207140310017c4d52fb96e70ced6cce
Ole Kallerud <ole@kallerud.no>
2024-12-26 22:59:05 +0100

    Implement progress bar when downloading data

--- src/providers/AppDataProvider.tsx
+++ src/providers/AppDataProvider.tsx
@@ -1,4 +1,3 @@
-import FetchErrorView from '@/src/components/FetchErrorView'
 import { CardManager } from '@/src/managers/CardManager'
 import { CardRelationManager } from '@/src/managers/CardRelationManager'
 import { CategoryManager } from '@/src/managers/CategoryManager'
@@ -9,8 +8,11 @@ import { PackManager } from '@/src/managers/PackManager'
 import SupabaseManager, { SupabaseItem } from '@/src/managers/SupabaseManager'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { ReactNode, useEffect, useRef } from 'react'
-import { AppState, Platform } from 'react-native'
-import ActivityIndicatorView from '../components/ActivityIndicatorView'
+import { AppState, Platform, View, ViewProps } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import FetchErrorView from '../components/FetchErrorView'
+import Colors from '../constants/Colors'
+import Color from '../models/Color'
 
 function useSupabase(manager: SupabaseManager<SupabaseItem>, enabled = true) {
   const response = useQuery({
@@ -36,7 +38,18 @@ export default function AppDataProvider(props: Readonly<{ children: ReactNode }>
   const packResponse = useSupabase(PackManager, languageResponse.isSuccess)
   const localizationResponse = useSupabase(LocalizationManager, languageResponse.isSuccess)
 
+  const responses = [
+    configResonse,
+    languageResponse,
+    categoryResponse,
+    packResponse,
+    cardResponse,
+    cardRelationResponse,
+    localizationResponse,
+  ]
+
   useEffect(() => {
+    // For Android: Handle user changing language in settings
     const stateListener = AppState.addEventListener('change', nextAppState => {
       if (
         Platform.OS === 'android' &&
@@ -56,29 +69,58 @@ export default function AppDataProvider(props: Readonly<{ children: ReactNode }>
     return () => stateListener.remove()
   }, [])
 
-  const isSuccess =
-    configResonse.isSuccess &&
-    languageResponse.isSuccess &&
-    categoryResponse.isSuccess &&
-    packResponse.isSuccess &&
-    cardResponse.isSuccess &&
-    cardRelationResponse.isSuccess &&
-    localizationResponse.isSuccess
+  if (responses.some(r => r.isError)) {
+    const errors = responses.reduce((acc, r) => (r.error ? [...acc, r.error] : acc), new Array<Error>())
+    return <FetchErrorView errors={errors} onRetry={() => queryClient.refetchQueries()} />
+  }
 
-  const failedResponses = [
-    configResonse,
-    languageResponse,
-    categoryResponse,
-    packResponse,
-    cardResponse,
-    cardRelationResponse,
-    localizationResponse,
-  ].filter(response => !!response.error) as { error: Error }[]
+  if (queryClient.isFetching() || responses.some(r => r.isPending)) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ProgressBar progress={responses.filter(r => !r.isPending).length / (responses.length - 2)} />
+      </View>
+    )
+  }
 
-  if (failedResponses.length > 0)
-    return <FetchErrorView errors={failedResponses.map(r => r.error)} onRetry={() => queryClient.invalidateQueries()} />
-
-  if (!isSuccess || queryClient.isFetching())
-    return <ActivityIndicatorView text={LocalizationManager.get('loading')?.value} />
   return props.children
+}
+
+function ProgressBar(props: Readonly<{ progress: number } & ViewProps>) {
+  const { progress, style } = props
+  const animationProgress = useSharedValue(progress)
+
+  const animatedProgressStyle = useAnimatedStyle(() => {
+    return {
+      width: `${animationProgress.value * 100}%`,
+    }
+  }, [progress])
+
+  useEffect(() => {
+    animationProgress.value = withTiming(Math.max(Math.min(progress, 1), 0), { duration: 50 })
+  }, [progress])
+
+  return (
+    <View
+      style={[
+        {
+          height: 8,
+          width: '50%',
+          backgroundColor: Color.hex(Colors.accentColor).alpha(0.25).string,
+          borderRadius: Number.MAX_SAFE_INTEGER,
+          overflow: 'hidden',
+        },
+        style,
+      ]}
+    >
+      <Animated.View
+        style={[
+          {
+            height: '100%',
+            backgroundColor: Colors.accentColor,
+          },
+          animatedProgressStyle,
+        ]}
+      />
+    </View>
+  )
 }
