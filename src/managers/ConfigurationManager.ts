840171a5b40889d51092e8f3c1f744a22de4480c
Ole <ole@kallerud.no>
2025-01-03 17:40:57 +0100

    Make configuration keys type safe

--- src/managers/ConfigurationManager.ts
+++ src/managers/ConfigurationManager.ts
@@ -2,11 +2,26 @@ import SupabaseManager from './SupabaseManager'
 import { Tables } from '@/src/models/supabase'
 
 const tableName = 'configurations'
-export type Configuration = Tables<typeof tableName>
+export type Configuration = Tables<typeof tableName> & { key: ConfigurationKey }
+export type ConfigurationKey =
+  | 'app_store_url'
+  | 'bottom_sheet_min_position'
+  | 'brisen_plus_id'
+  | 'default_gradient'
+  | 'default_language'
+  | 'max_simultaneous_open_cards'
+  | 'max_unclosed_card_age'
+  | 'play_store_url'
+  | 'sfw_language'
+  | 'use_sfw_content'
 
 class ConfigurationManagerSingleton extends SupabaseManager<Configuration> {
   constructor() {
     super(tableName)
+  }
+
+  get(key: ConfigurationKey) {
+    return this._items?.get(key)
   }
 }
 
