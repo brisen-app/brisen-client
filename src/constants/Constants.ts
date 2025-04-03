b1a4b8c930dca20baace43872e063de19c6acf83
Ole Kallerud <ole@kallerud.no>
2025-04-03 20:32:51 +0200

    Bump version to 1.1.1

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,6 +1,6 @@
 export const majorVersion = 1 // Marketing version, requires a new build
 export const minorVersion = 1 // Requires a new build
-export const patchVersion = 0 // Doesn't require a new build
+export const patchVersion = 1 // Doesn't require a new build
 
 export const appVersion = `${majorVersion}.${minorVersion}.${patchVersion}`
 export const runtimeVersion = `${majorVersion}.${minorVersion}`
